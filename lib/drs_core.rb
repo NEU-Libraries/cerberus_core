6eb9b8e4de5fd60efd738dfae0e45846b807cb19
Change Me <change@me.com>
2014-07-25 12:27:37 -0400

    Updates

--- lib/drs_core.rb
+++ lib/drs_core.rb
@@ -1,2 +1,13 @@
+require 'active-fedora'
+require 'hydra/head'
+require 'hydra/derivatives'
+
 module DrsCore
+  require 'drs_core/datastreams/drs_properties_datastream'
+  require 'drs_core/datastreams/file_content_datastream'
+  require 'drs_core/datastreams/northeastern_dublin_core_datastream'
+  require 'drs_core/datastreams/paranoid_rights_datastream'
+
+  require 'drs_core/content_object'
+  require 'drs_core/core_record'
 end
