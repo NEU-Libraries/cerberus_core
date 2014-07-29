76b78a4d387d1ca45793c8861da63150ab9ef985
Change Me <change@me.com>
2014-07-29 12:37:20 -0400

    Move test properties datastream into the right subdir.

--- spec/dummy/app/models/datastreams/test_properties_datastream.rb
+++ spec/dummy/app/models/datastreams/test_properties_datastream.rb
@@ -2,6 +2,10 @@ class TestPropertiesDatastream < DrsCore::Datastreams::PropertiesDatastream
   use_terminology DrsCore::Datastreams::PropertiesDatastream 
 
   extend_terminology do |t| 
-    t.test_attribute(path: 'test_attribute', namespace: 'dc')
+    t.test_attribute(path: 'testAttribute', namespace: 'dc')
+  end
+
+  def get_depositor 
+    self.depositor.first
   end
 end
