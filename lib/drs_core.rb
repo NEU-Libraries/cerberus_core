4957ac8d4b85455d088cd5bd38eb82c3364a2c3c
Change Me <change@me.com>
2014-07-29 10:35:29 -0400

    Properties datastream first cut.

--- lib/drs_core.rb
+++ lib/drs_core.rb
@@ -3,7 +3,7 @@ require 'hydra/head'
 require 'hydra/derivatives'
 
 module DrsCore
-  require 'drs_core/datastreams/drs_properties_datastream'
+  require 'drs_core/datastreams/properties_datastream'
   require 'drs_core/datastreams/file_content_datastream'
   require 'drs_core/datastreams/northeastern_dublin_core_datastream'
   require 'drs_core/datastreams/paranoid_rights_datastream'
