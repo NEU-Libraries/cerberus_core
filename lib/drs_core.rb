cab17080083751480490732af9bff2d7e41fa31e
Will Jackson <wjackson64@gmail.com>
2014-07-29 13:14:02 -0400

    Remove paranoid rights Datastream from project -> feels like this should be replaced with a call to Hydra::AccessControls::Permissions.

--- lib/drs_core.rb
+++ lib/drs_core.rb
@@ -5,8 +5,7 @@ require 'hydra/derivatives'
 module DrsCore
   require 'drs_core/datastreams/properties_datastream'
   require 'drs_core/datastreams/file_content_datastream'
-  require 'drs_core/datastreams/northeastern_dublin_core_datastream'
-  require 'drs_core/datastreams/paranoid_rights_datastream'
+  require 'drs_core/datastreams/dublin_core_datastream'
   require 'drs_core/datastreams/nu_mods_datastream'
 
   require 'drs_core/content_object'
