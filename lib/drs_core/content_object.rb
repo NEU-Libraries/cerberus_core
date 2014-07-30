6b6607ee31e337fd6f4301df33cf8c391ec7ce64
Will Jackson <wjackson64@gmail.com>
2014-07-30 15:18:55 -0400

    Adds characterization logic to objects of the content_object type.

--- lib/drs_core/content_object.rb
+++ lib/drs_core/content_object.rb
@@ -3,17 +3,16 @@ module DrsCore::ContentObject
   extend ActiveSupport::Concern
 
   included do
-    include DrsCore::Concerns::ParanoidRightsValidation
-
     include Hydra::ModelMixins::RightsMetadata
     include Hydra::ModelMethods
 
     include DrsCore::Concerns::PropertiesDatastreamDelegations
+    include DrsCore::Concerns::Characterizable
 
     has_metadata name: 'DC', type: DrsCore::Datastreams::DublinCoreDatastream
     has_metadata name: 'rightsMetadata', type: DrsCore::Datastreams::ParanoidRightsDatastream
     has_metadata name: 'properties', type: DrsCore::Datastreams::PropertiesDatastream 
-    has_metadata name: 'fits', type: DrsCore::Datastreams::FitsDatastream
+    has_metadata name: 'characterization', type: DrsCore::Datastreams::FitsDatastream
     has_file_datastream name: 'content', type: DrsCore::Datastreams::FileContentDatastream 
   end
 
