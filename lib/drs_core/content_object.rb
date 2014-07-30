fef74924858fef9f5d8ece1b7c9b3b9ba3c721e9
Will Jackson <wjackson64@gmail.com>
2014-07-30 11:20:06 -0400

    Get content object working-ish again.

--- lib/drs_core/content_object.rb
+++ lib/drs_core/content_object.rb
@@ -2,21 +2,17 @@
 module DrsCore::ContentObject
   extend ActiveSupport::Concern
 
-  # Provides permissions logic for accessing read/edit groups/persons, 
-  # Setting read/edit groups/persons, etc.
-  include Hydra::ModelMixins::RightsMetadata
+  included do
+    include Hydra::ModelMixins::RightsMetadata
+    include Hydra::ModelMethods
 
-  # Primarily useful for adding the add_file method
-  include Hydra::ModelMethods
-
-  included do 
-    # has_metadata name: 'DC', type: DrsCore::Datastreams::NortheasternDublinCoreDatastream
-    # has_metadata name: 'rightsMetadata', type: DrsCore::Datastreams::ParanoidRightsDatastream
-    # has_metadata name: 'properties', type: DrsCore::Datastreams::DrsPropertiesDatastream 
-    # has_file_datastream name: 'content', type: DrsCore::Datastreams::FileContentDatastream 
+    has_metadata name: 'DC', type: DrsCore::Datastreams::DublinCoreDatastream
+    has_metadata name: 'rightsMetadata', type: DrsCore::Datastreams::ParanoidRightsDatastream
+    has_metadata name: 'properties', type: DrsCore::Datastreams::PropertiesDatastream 
+    has_file_datastream name: 'content', type: DrsCore::Datastreams::FileContentDatastream 
   end
 
   def type_label
-    self.class.name 
+    self.class.name
   end
 end
