7d7d32dd55d3383b17088d1feec489e829420118
Will Jackson <wjackson64@gmail.com>
2014-07-30 09:48:48 -0400

    Get rid of documentation for the moment - annoying.

--- lib/drs_core/core_record.rb
+++ lib/drs_core/core_record.rb
@@ -6,19 +6,17 @@
 module DrsCore::CoreRecord
   extend ActiveSupport::Concern 
 
-  included do 
+  included do
+    include DrsCore::Concerns::ParanoidRightsValidation
+    include Hydra::ModelMixins::RightsMetadata
+
     before_destroy :destroy_content_objects
 
     CONTENT_CLASSES = nil #Add an array of content class names here
 
-    # has_metadata name: "DC", type: DrsCore::Datastreams::NortheasternDublinCoreDatastream
-    # has_metadata name: "rightsMetadata", type: DrsCore::Datastreams::ParanoidRightsDatastream
-    # has_metadata name: "properties", type: DrsCore::Datastreams::DrsPropertiesDatastream
-    # has_metadata name: "mods", type: DrsCore::Datastreams::NuModsDatastream
-
     # Every CoreRecord class should specify an array of model names
     # as strings for the content objects that can exist off this CoreRecord
-    # under the instance_variable @content_classes.  E.g. 
+    # under the constant CONTENT_CLASSES.  E.g. 
     # a CoreRecord class that has AudioFile and VideoFile content object children
     # would specify @content_classes = ["AudioFile", "VideoFile"]
     def self.content_classes
@@ -29,9 +27,8 @@ module DrsCore::CoreRecord
   # Fetches all content objects that are attached to this core record (using solr) 
   # and returns them cast to their fedora model objects
   def content_objects
-    a = self.class.content_classes
 
-    a.inject! { |base, str| base + "OR \"#{str}\""}
+    content_classes.inject! { |base, str| base + "OR \"#{str}\""}
     models = "active_fedora_model_ssi:(#{a})"
     belongs_to_this = "is_part_of_ssim:\"info:fedora/#{self.pid}\""
 
