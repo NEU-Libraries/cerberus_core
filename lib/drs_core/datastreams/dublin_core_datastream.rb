dec4e7a36a9bbdddce91a089b161a3e0848762cc
Will Jackson <wjackson64@gmail.com>
2014-07-31 13:04:38 -0400

    Update dublin_core_datastream to implement the entire schema for unqualified dublin core.

--- lib/drs_core/datastreams/dublin_core_datastream.rb
+++ lib/drs_core/datastreams/dublin_core_datastream.rb
@@ -1,60 +1,38 @@
 module DrsCore::Datastreams
+  # Implements the entire quite basic schema for Unqualified Dublin Core.
   class DublinCoreDatastream < ActiveFedora::OmDatastream
     include OM::XML::Document
 
-    set_terminology do |t| 
+    # :nodoc:
+    # Boy I hate typing. AH = Attributes Hash, for the record
+    AH = {namespace_prefix: 'dc'}
+
+    set_terminology do |t|
       t.root(path: 'dc', namespace_prefix: 'oai_dc', 'xmlns:oai_dc' => 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'xmlns:dc' => 'http://purl.org/dc/elements/1.1/', 'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation' => 'http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd')
-      t.title(path: 'title', namespace_prefix: 'dc')
-      t.description(path: 'description', namespace_prefix: 'dc') 
-      t.type(path: 'type', namespace_prefix: 'dc')
-      t.identifier(path: 'identifier', namespace_prefix: 'dc')
-      t.date(namespace_prefix: 'dc')
-      t.creator(namespace_prefix: 'dc') 
-      t.subject(namespace_prefix: 'dc') 
+      t.title(AH)
+      t.creator(AH) 
+      t.subject(AH) 
+      t.description(AH)
+      t.publisher(AH)
+      t.contributor(AH)
+      t.date(AH)
+      t.type(AH)
+      t.format(AH)  
+      t.identifier(AH)
+      t.source(AH)
+      t.language(AH)
+      t.relation(AH)
+      t.coverage(AH)
+      t.rights(AH)
     end
 
     def self.xml_template
       builder = Nokogiri::XML::Builder.new do |xml| 
         xml['oai_dc'].dc('xmlns:oai_dc' => 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'xmlns:dc' => 'http://purl.org/dc/elements/1.1/', 'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance', 
                       'xsi:schemaLocation' => 'http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd'){
-          xml['dc'].title
-          xml['dc'].creator 
-          xml['dc'].date 
-          xml['dc'].description
-          xml['dc'].type
-          xml['dc'].subject
-          xml['dc'].identifier
         }
       end
       builder.doc
-    end
-
-    # With no method of distinguishing between corporate and personal creators,
-    # we have to collapse assignment of all creators into a single method to avoid 
-    # unintentional overwrites.  This is not ideal.  
-    def assign_creators(first_names, last_names, corporate_creators)
-      pns = construct_personal_names(first_names, last_names) 
-
-      all_names = pns + corporate_creators
-
-      self.creator = all_names
-    end
-
-
-    # Individual contributors/creators will typically be identified by first/last name.
-    # This method takes two arrays of equal length and creates personal names with them. 
-    def construct_personal_names(first_names, last_names)
-
-      if first_names.length != last_names.length 
-        raise "Passed #{first_names.length} first names and #{last_names.length} last names."
-      end
-
-      full_names = []
-      first_names.each_with_index do |fn, i| 
-       full_names << "#{fn} #{last_names[i]}" 
-      end
-
-      return full_names
     end
   end
 end
--- spec/dummy/spec/models/datastreams/dublin_core_datastream_spec.rb
+++ spec/dummy/spec/models/datastreams/dublin_core_datastream_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper' 
 
 describe DrsCore::Datastreams::DublinCoreDatastream do 
-
   pending "write some tests" 
 end
