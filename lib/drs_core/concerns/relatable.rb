279f0003be5183fb3af50b87b9aee8d6a5587d94
Will Jackson <wjackson64@gmail.com>
2014-07-31 14:59:16 -0400

    Adds a module for implementing the relation assertion helper, adds collection skeleton.

---
+++ lib/drs_core/concerns/relatable.rb
@@ -0,0 +1,13 @@
+module DrsCore::Concerns::Relatable 
+  extend ActiveSupport::Concern 
+
+  module ClassMethods
+    def relation_asserter(method, rel_name, rel_type, rel_class)
+      if rel_class 
+        self.send(method, rel_name, :property => rel_type, :class => rel_class)
+      else
+        self.send(method, rel_name, :property => rel_type)
+      end
+    end
+  end
+end
