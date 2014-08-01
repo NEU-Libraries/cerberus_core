93763ec04d2622d5ebc1bd4638fcdf4db29abea5
Will Jackson <wjackson64@gmail.com>
2014-08-01 15:07:09 -0400

    rename constant FOLDER_CLASSES to the more consistent COLLECTION_CLASSES

--- spec/dummy/app/models/kollection.rb
+++ spec/dummy/app/models/kollection.rb
@@ -3,5 +3,5 @@ class Kollection < DrsCore::BaseModels::Collection
   relate_to_parent_collection(:parent_kollection, "Kollection")
 
   CORE_RECORD_CLASSES = ["CoreFile"]
-  FOLDER_CLASSES      = ["Kollection"]
+  COLLECTION_CLASSES  = ["Kollection"]
 end
