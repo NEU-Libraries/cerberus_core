08960483d645d71686cfabe3be54ae22d26661fc
Will Jackson <wjackson64@gmail.com>
2014-08-04 11:08:05 -0400

    Write some documentation for concerns.

--- lib/drs_core/concerns/relatable.rb
+++ lib/drs_core/concerns/relatable.rb
@@ -1,7 +1,13 @@
+# Mostly internal helper module.
 module DrsCore::Concerns::Relatable 
   extend ActiveSupport::Concern 
 
   module ClassMethods
+    # A convenience method for defining the relate_to_x methods found in 
+    # the Community and Collection base models, which are themselves convenience
+    # methods over the relationship assertions provided by ActiveFedora.  Useful
+    # primarily for enforcing (sorta) the expected relationships tying the graph 
+    # together. 
     def relation_asserter(method, rel_name, rel_type, rel_class)
       if rel_class 
         self.send(method, rel_name, :property => rel_type, :class_name => rel_class)
--- lib/drs_core/concerns/traversals.rb
+++ lib/drs_core/concerns/traversals.rb
@@ -1,36 +1,64 @@
-module DrsCore::Concerns::Traversals 
+# Implements traversals over the graph of core records, collections, 
+# and communities.  Included in Collections and Communities and in 
+# SolrDocumentBehavior.
+# ==== Options
+# All the methods defined in this module take the following options: 
+# * +:return_as+ - A symbol dictating how the results of the Solr query 
+#   ought to be returned to the user.  The default option is 
+#   :query_result, which simply returns the array of solr responses
+#   retrieved by ActiveFedora::SolrService.query().  Other options are
+#   :models, which casts each result to its fedora object model, and 
+#   :solr_document, which returns an array of SolrDocuments. 
+module DrsCore::Concerns::Traversals
+  # Creates a new QueryService object from the given object. 
+  # Ought to know how to create from a fedora level model, 
+  # a SolrDocument, or a raw response hash.  
   def new_query
     DrsCore::Services::QueryService.create_from_object(self)
   end
 
+  # Fetch all children (immediate descendents) of this fedora object.
   def children(opts)
     new_query.get_children opts
   end
 
+  # Fetch all descendents of this fedora object.
   def descendents(opts) 
     new_query.get_descendents opts 
   end
 
+  # Fetch all children which are named in CORE_RECORD_CLASSES
+  # for this fedora object.
   def records(opts) 
     new_query.get_child_records opts
   end
 
+  # Fetch all descendents which are named in CORE_RECORD_CLASSES
+  # for this fedora object.
   def descendent_records(opts) 
     new_query.get_descendent_records opts 
   end
 
+  # Fetch all children which are named in COLLECTION_CLASSES
+  # for this fedora object.
   def collections(opts) 
     new_query.get_child_collections opts 
   end
 
+  # Fetch all descendents which are named in COLLECTION_CLASSES
+  # for this fedora object.
   def descendent_collections(opts) 
     new_query.get_descendent_collections opts 
   end
 
+  # Fetch all children which are named in COMMUNITY_CLASSES
+  # for this fedora object.
   def communities(opts) 
     new_query.get_child_communities opts
   end
 
+  # Fetch all descendents which are named in COMMUNITY_CLASSES
+  # for this fedora object.
   def descendent_communities(opts)
     new_query.get_descendent_communities opts
   end
