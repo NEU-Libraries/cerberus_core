0548d5d5485f18670ee5f87274a5ae2c30ce4275
Will Jackson <wjackson64@gmail.com>
2014-08-04 15:47:04 -0400

    Tests for properties datastream and rights metadata datastream accessors on solr documents.

--- spec/dummy/spec/models/solr_document_spec.rb
+++ spec/dummy/spec/models/solr_document_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe SolrDocument do 
+  let(:core_file) { CoreFile.new} 
+
+  def doc 
+    SolrDocument.new(core_file.to_solr) 
+  end
 
   describe "Traversals" do 
     let(:doc) { SolrDocument.new(@community.to_solr) } 
@@ -34,6 +39,59 @@ describe SolrDocument do
       @community.destroy
       @collection.destroy 
       @core_file.destroy 
+    end
+  end
+
+  describe "Rights metadata access" do 
+    it "allows us to check an object's mass permissions" do
+      expect(doc.is_private?).to be true
+
+      core_file.read_groups = ['public']
+      expect(doc.is_public?).to be true 
+
+      core_file.read_groups = ['registered']
+      expect(doc.is_registered?).to be true 
+    end
+
+    it "allows us to see an object's read users" do 
+      core_file.read_users = ["Will"]
+      expect(doc.read_people).to eq ["Will"]
+    end
+
+    it "allows us to see an object's edit users" do 
+      core_file.edit_users = ["Will"] 
+      expect(doc.edit_people).to eq ["Will"]
+    end
+
+    it "allows us to see an object's edit groups" do 
+      core_file.edit_groups = ["Willump"]
+      expect(doc.edit_groups).to eq ["Willump"]
+    end
+
+    it "allows us to see an object's read groups" do 
+      core_file.read_groups = ["Willump"]
+      expect(doc.read_groups).to eq ["Willump"]
+    end
+  end
+
+  describe "Properties datastream access" do 
+    it "allows us to read an objects depositor" do 
+      core_file.apply_depositor_metadata "Will" 
+      expect(doc.depositor).to eq "Will" 
+    end
+
+    it "allows us to check whether an object is in progress" do 
+      core_file.tag_as_in_progress
+      expect(doc.in_progress?).to be true
+
+      core_file.tag_as_completed
+      expect(doc.in_progress?).to be false
+    end
+
+    it "allows us to check if this object is canonical" do 
+      expect(doc.canonical?).to be false 
+      core_file.canonize
+      expect(doc.canonical?).to be true 
     end
   end
 end
