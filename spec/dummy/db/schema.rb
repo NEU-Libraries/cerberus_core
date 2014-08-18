8857dd1885dc9464f20570cf46b62c8826466a2e
Change Me <change@me.com>
2014-08-18 10:13:02 -0400

    Add users

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -13,4 +13,11 @@
 
 ActiveRecord::Schema.define(:version => 20140724204341) do
 
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "nuid"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
 end
