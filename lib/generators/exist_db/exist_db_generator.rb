57cd5c0eda1c4abee29946b51264a4bf3e093ccb
Will Jackson <wjackson64@gmail.com>
2014-08-21 15:28:55 -0400

    Add curb to project

--- lib/generators/exist_db/exist_db_generator.rb
+++ lib/generators/exist_db/exist_db_generator.rb
@@ -1,3 +1,15 @@
 class ExistDbGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+
+  def insert_war_file
+    #TODO
+  end
+
+  def insert_context_file
+
+  end
+
+  def insert_config_file
+
+  end
 end
