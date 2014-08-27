a5643bb24eafe607807f236f7da76d11c5eb2465
Will Jackson <wjackson64@gmail.com>
2014-08-27 11:09:55 -0400

    Add base models generator to project.

--- lib/generators/cerberus_core/exist_generator.rb
+++ lib/generators/cerberus_core/exist_generator.rb
@@ -2,6 +2,20 @@ module CerberusCore
   class ExistGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
+    desc <<-eos 
+    Description:
+      Given a preexisting hydra-jetty installation (rails g hydra:jetty), this
+      generator sets up an instance of eXist-db to run alongside Fedora and Solr.
+
+    Example:
+      rails generate cerberus_core:exist
+
+    This will create:
+        jetty/webapps/exist-2.2.rev.war
+        jetty/contexts/exist.xml
+        config/exist_db.yml
+    eos
+
     def insert_war_file
       #TODO
     end
